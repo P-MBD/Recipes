19880ac425d003671aa803c1a74d01be57b112a3
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-29 07:04:09 +0330

    1. Customizing Navigation Header

--- App.js
+++ App.js
@@ -10,9 +10,9 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerTitleAlign:'center'}}>
         <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Home} options={{headerLeft:null, gestureEnabled:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );

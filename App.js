d23728436a6035742015060531b327cc5968cff7
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-29 08:44:11 +0330

    3. Changing App Theme

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Image, Pressable } from 'react-native';
+import { Image, Pressable, StyleSheet } from 'react-native';
 import Splash from './src/screens/Splash';
-import Search from './src/screens/Search';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/screens/Home';
+import Search from './src/screens/Search';
 
 const Stack = createStackNavigator();
+
 const BackButton = (props) => {
-  console.log('props->', props);
-  return(
+  return (
     <Pressable onPress={props.onPress}>
-        <Image style={styles.back} source={require('./assets/back.png')}/>
+      <Image style={styles.back} source={require('./assets/back.png')} />
     </Pressable>
   )
 }
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#ffffff',
+  },
+}
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerTitleAlign:'center'}}>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{ headerTitleAlign: 'center', headerShadowVisible: false }}>
         <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={Home} options={{headerLeft:null, gestureEnabled:false}} />
-        <Stack.Screen name="Search" component={Search} options={{headerLeft:(props) => <BackButton {... props} />}} />
+        <Stack.Screen name="Home" component={Home} options={{ headerLeft: null, gestureEnabled: false }} />
+        <Stack.Screen name="Search" component={Search} options={{ headerLeft: (props) => <BackButton {...props} /> }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -30,7 +38,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   back: {
-    width:36,
-    height:36,
+    width: 24,
+    height: 24,
+    margin: 16,
   },
 });

320c2df5ee327d135588fe8e8a21319a40d393fd
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-07-31 08:02:50 +0330

    10. Show Recipes in List

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text,SafeAreaView} from 'react-native';
+import {View, Text,SafeAreaView,FlatList} from 'react-native';
 import Categories from '../../components/Categories';
 import Input from '../../components/Input';
 import RecipeCard from '../../components/RecipeCard';
@@ -11,14 +11,25 @@ const Home= ({navigation})=>{
         <SafeAreaView style={styles.container}>
              <Input pressable onPress= {()=> navigation.navigate('Search')} />
              <Title text= "Featured Recipes"/>
-             <RecipeCard
-                  title="Steak with tomato sauce and bulgur rice."
-                  time="20 mins"
-                  author={{
-                      name: 'James Milner',
-                      image: 'https://lwlies.com/wp-content/uploads/2017/04/avatar-2009.jpg'
-                  }}
-              />
+          
+             <FlatList 
+                horizontal
+                data={[1,2,3]} 
+                style={{marginHorizontal: -24}}
+                keyExtractor={item => String(item)}
+                showsHorizontalScrollIndicator={false}
+                renderItem={({index}) => (
+                    <RecipeCard
+                        style={index === 0 ? { marginLeft: 24 } : {}}
+                        title="Steak with tomato sauce and bulgur rice."
+                        time="20 mins"
+                        author={{
+                            name: 'James Milner',
+                            image: 'https://lwlies.com/wp-content/uploads/2017/04/avatar-2009.jpg'
+                        }}
+                    />
+                )}
+            />
              <Categories categories={["All","Trending"]} selectedCategory="All" onCategoryPress={()=>{}}/>
              
              
